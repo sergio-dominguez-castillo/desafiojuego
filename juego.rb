926aff92d8ddacde1d5f63ce69769f2cedcfe899
Sergio Dominguez <sergio.castle@gmail.com>
2023-06-17 00:54:49 -0400

    commit agrego rutina de valor al azar del computador

--- juego.rb
+++ juego.rb
@@ -17,6 +17,8 @@ while validacion == 0
                 option = 0 if valor == 'piedra'
                 option = 1 if valor == 'papel'
                 option = 2 if valor == 'tijera'  
+                # envio mensaje a la consola con la opcion del jugador
+                puts "Jugador juega #{valor}" 
                 # cambio valor para romper el ciclo
                 validacion=1  
             end
@@ -24,10 +26,18 @@ while validacion == 0
         # cargo variable para jugar 
         option = 0 if arg == 'piedra'
         option = 1 if arg == 'papel'
-        option = 2 if arg == 'tijera'  
+        option = 2 if arg == 'tijera'
+        # envio mensaje a la consola con la opcion del jugador
+        puts "Jugador juega #{arg}" 
         # cambio valor para romper el ciclo
         validacion=1
     end
 end
-
+# ahora asigno valor al azar del computador
+  rand_number = rand(0-3)
+  computer_option = 'piedra' if rand_number == 0
+  computer_option = 'papel' if rand_number == 1
+  computer_option = 'tijera' if rand_number == 2
+# envio mensaje a la consola con la opcion del computador
+  puts "Computador juega #{computer_option}"
 
