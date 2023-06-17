7c41e334f5139aca8ea897551d0bd2426e36214b
Sergio Dominguez <sergio.castle@gmail.com>
2023-06-17 00:36:50 -0400

    commit prueba unitarias validaciones de valor de entrada

--- juego.rb
+++ juego.rb
@@ -1,28 +1,25 @@
 arg = ARGV[0]
 
 validacion=0
-while validacion = 0
+while validacion == 0
     
     # reviso valor de entrada
     if arg != 'tijera' && arg != 'piedra' && arg != 'papel'
-    
         # en caso de no corresponder, se lo pido en forma interactiva
-    puts 'Para jugar, ingresar valor entre; papel, piedra o tijera'
-    valor = gets
-
-        if valor != 'tijera' && valor != 'piedra' && valor != 'papel'
-
-        # envio mensaje de error
-        puts "Argumento inválido: Debe ser piedra, papel o tijera"
-        
-        else
-            # cargo variable para jugar 
-            option = 0 if valor == 'tijera'
-            option = 1 if valor == 'piedra'
-            option = 2 if valor == 'papel'  
-            # cambio valor para romper el ciclo
-            validacion=1  
-        end
+        puts 'Para jugar, ingresar valor entre; papel, piedra o tijera'
+        valor=STDIN.gets.chomp
+            # çvalido en valor ingresado
+            if valor != 'tijera' && valor != 'piedra' && valor != 'papel'
+                # envio mensaje de error
+                puts "Argumento inválido: Debe ser piedra, papel o tijera"
+            else
+                # cargo variable para jugar 
+                option = 0 if valor == 'tijera'
+                option = 1 if valor == 'piedra'
+                option = 2 if valor == 'papel'  
+                # cambio valor para romper el ciclo
+                validacion=1  
+            end
     else
         # cargo variable para jugar 
         option = 0 if arg == 'tijera'
@@ -31,5 +28,6 @@ while validacion = 0
         # cambio valor para romper el ciclo
         validacion=1
     end
+end
 
 
